e7f24de7caf6aa4fb15466ad6f3820b8bb60027f
Quranov <knswork@mail.ru>
2025-07-06 18:35:43 +0300

    gtest

--- test_version.cpp
+++ test_version.cpp
@@ -1,13 +1,11 @@
-#define BOOST_TEST_MODULE test_version
-
+#include <gtest/gtest.h>
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version() > 0);
+TEST(VersionTest, Tests1) {
+    EXPECT_TRUE(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
