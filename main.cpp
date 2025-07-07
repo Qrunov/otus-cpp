155c30add58da87b271deb66e8b0f5c69ebcc527
Quranov <knswork@mail.ru>
2025-07-07 08:13:44 +0300

    add files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
-#include "lib.h"
+#include "ip_filter.h"
 
-#include <iostream>
+int main(int, char **)
+{
+	auto [r, ipvec] = ipParser::parseStream(cin);
+	if (ipParser::Error_code::no_error != r)
+	{
+		cerr << "parse error has occured";
+		return -1;
+	}
+	auto ip_cmp_gt = [](const ip &ip1, const ip &ip2)
+	{
+		return !(ip1 <= ip2);
+	};
+	sort(ipvec.begin(), ipvec.end(), ip_cmp_gt);
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	auto output = [](const ip &out)
+	{
+		cout << out.get_string() << endl;
+	};
+
+	auto f1 = [](const ip &out)
+	{
+		if (1 == out.get_field(1))
+			cout << out.get_string() << endl;
+	};
+
+	auto f2 = [](const ip &out)
+	{
+		if (46 == out.get_field(1) && 70 == out.get_field(2))
+			cout << out.get_string() << endl;
+	};
+
+	auto f3 = [](const ip &out)
+	{
+		if (46 == out.get_field(1) || 46 == out.get_field(2) || 46 == out.get_field(3) || 46 == out.get_field(4))
+			cout << out.get_string() << endl;
+	};
+
+	array<function<void(const ip &)>, 4> ls{output, f1, f2, f3};
+
+	for (const auto &do_out : ls)
+		for_each(ipvec.begin(), ipvec.end(), do_out);
+
 	return 0;
 }
--- test_version.cpp
+++ test_version.cpp
@@ -1,9 +1,52 @@
 #include <gtest/gtest.h>
-#include "lib.h"
+#include "ip_filter.h"
 
-TEST(VersionTest, Tests1) {
-    EXPECT_TRUE(version() > 0);
+TEST(Test1, TestsIpClass) {
+    ip _ip;
+	_ip.set_field(1, 10);
+	_ip.set_field(2, 20);
+	_ip.set_field(3, 30);
+	_ip.set_field(4, 40);
+	EXPECT_STREQ(_ip.get_string().c_str(),"10.20.30.40");
 }
+
+
+TEST(Test2, TestsParser) {
+    stringstream ss;
+	ss << "192.168.3.4\t5\t66\n192.168.5.5\t555\t6767\n";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::no_error);
+}
+
+TEST(Test3, TestsParser) {
+    stringstream ss;
+	ss << "192.168.333.4\t5\t66\n192.168.5.5\t555\t6767\n";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::range_error);
+}
+
+TEST(Test4, TestsParser) {
+    stringstream ss;
+	ss << "192.168.3.7.8.4\t5\t66\n192.168.5.5\t555\t6767\n";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::input_error);
+}
+
+TEST(Test5, TestsParser) {
+    stringstream ss;
+	ss << "192.168.3.7\t66\n192.168.5.5\t555\t6767\n";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::input_error);
+}
+
+TEST(Test6, TestsParser) {
+    stringstream ss;
+	ss << "192.168.3.7";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::input_error);
+}
+
+TEST(Test7, TestsParser) {
+    stringstream ss;
+	ss << "192.168:3.7\t5\t66\n192.168.5.5\t555\t6767\n";
+	EXPECT_EQ(get<0>(ipParser::parseStream(ss)),ipParser::Error_code::input_error);
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
