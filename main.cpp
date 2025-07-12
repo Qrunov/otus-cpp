b917015a54de6103ce6e938a52050dc3db9057df
Quranov <knswork@mail.ru>
2025-07-12 17:17:49 +0300

    tuple

--- main.cpp
+++ main.cpp
@@ -1,46 +1,42 @@
-#include "ip_filter.h"
+#include <string>
+#include <tuple>
+#include <cassert>
 
-int main(int, char **)
+template <typename... Ts>
+class custom_tie
 {
-	auto [r, ipvec] = ipParser::parseStream(cin);
-	if (ipParser::Error_code::no_error != r)
-	{
-		cerr << "parse error has occured";
-		return -1;
-	}
-	auto ip_cmp_gt = [](const ip &ip1, const ip &ip2)
-	{
-		return !(ip1 <= ip2);
-	};
-	sort(ipvec.begin(), ipvec.end(), ip_cmp_gt);
+      std::tuple<Ts&...> stored;
+public:
+      custom_tie(Ts&... args): stored(args...) {};
+      int operator=(const std::tuple<Ts...> &t)
+      {
+          stored = t;
+          return 0;
+      }
+};
 
-	auto output = [](const ip &out)
-	{
-		cout << out.get_string() << endl;
-	};
-
-	auto f1 = [](const ip &out)
-	{
-		if (1 == out.get_field(1))
-			cout << out.get_string() << endl;
-	};
-
-	auto f2 = [](const ip &out)
-	{
-		if (46 == out.get_field(1) && 70 == out.get_field(2))
-			cout << out.get_string() << endl;
-	};
-
-	auto f3 = [](const ip &out)
-	{
-		if (46 == out.get_field(1) || 46 == out.get_field(2) || 46 == out.get_field(3) || 46 == out.get_field(4))
-			cout << out.get_string() << endl;
-	};
-
-	array<function<void(const ip &)>, 4> ls{output, f1, f2, f3};
-
-	for (const auto &do_out : ls)
-		for_each(ipvec.begin(), ipvec.end(), do_out);
-
-	return 0;
+auto getPerson() {
+ const std::string name = "Petia";
+ const std::string secondName = "Ivanoff";
+ const std::size_t age = 23;
+ const std::string department = "Sale";
+ return std::make_tuple(
+ name, secondName, age, department
+ );
 }
+int main(int, char **) {
+ std::string name, secondName, department;
+ std::size_t age;
+ custom_tie(name, secondName, age, department) = getPerson();
+
+ assert(name == "Petia");
+ assert(secondName == "Ivanoff");
+ assert(age == 23);
+ assert(department == "Sale");
+ return 0;
+}
+
+
+
+
+
