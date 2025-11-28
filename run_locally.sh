4fbbfa1b2c3bc6ec33945d864fc5777da66360eb
Quranov <knswork@mail.ru>
2025-11-28 19:20:24 +0300

    install run_locally.sh

--- run_locally.sh
+++ run_locally.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-BINARY_HOME=./
-INPUT_HOME=./input
-INPUT=${INPUT_HOME}/AB_NYC_2019.csv
+INPUT=/usr/local/share/AB_NYC_2019.csv
 
-cat ${INPUT} | ${BINARY_HOME}/mapper | sort -k1 | ${BINARY_HOME}/reducer1 > output1
+cat ${INPUT} | mapper | sort -k1 | reducer1 > output1
+echo -n "average:"
 cat output1
 
-cat ${INPUT} | ${BINARY_HOME}/mapper | sort -k1 | ${BINARY_HOME}/reducer2 > output2
+cat ${INPUT} | mapper | sort -k1 | reducer2 > output2
+echo -n "dispersion:"
 cat output2
