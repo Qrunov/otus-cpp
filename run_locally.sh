d4544fdf6239d2585e38e29d9d41e61d497e1349
kns <knswork@mail.ru>
2025-12-04 16:13:58 +0300

    diffrenet paths for install,build

--- run_locally.sh
+++ run_locally.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-INPUT=/usr/local/share/AB_NYC_2019.csv
 
-cat ${INPUT} | mapper | sort -k1 | reducer1 > output1
+# Ищем в стандартных местах
+for prefix in /usr /usr/local; do
+    DATA_FILE="$prefix/share/AB_NYC_2019.csv"
+    if [ -f "$DATA_FILE" ]; then
+        break
+    fi
+done
+
+
+DATA_FILE=/usr/local/share/AB_NYC_2019.csv
+
+cat ${DATA_FILE} | mapper | sort -k1 | reducer1 > output1
 echo -n "average:"
 cat output1
 
-cat ${INPUT} | mapper | sort -k1 | reducer2 > output2
+cat ${DATA_FILE} | mapper | sort -k1 | reducer2 > output2
 echo -n "dispersion:"
 cat output2
